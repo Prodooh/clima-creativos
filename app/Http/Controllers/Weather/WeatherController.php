5b5f07c7a50be7a482e2ae7350a82cec67431f98
cristoferprodooh <cristoferg@prodooh.com>
2023-01-31 17:31:21 -0600

    [FIX] add visual fixes

--- app/Http/Controllers/Weather/WeatherController.php
+++ app/Http/Controllers/Weather/WeatherController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Weather;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Http;
 
 class WeatherController extends Controller
@@ -15,6 +16,18 @@ class WeatherController extends Controller
     public function getWeather(){
         $url = env('OPEN_WEATHER_URL') . 'q=' . \request('venue') . '&appid=' . env('OPEN_WEATHER_MAP');
         $response = Http::get($url)->json();
+        if ($response['main']['temp'] < 15) {
+            File::copy(public_path('images/frio.jpg'), public_path('main.jpg'));
+        }
+
+        if ($response['main']['temp'] >= 15 && $response['main']['temp']<= 22)
+        {
+            File::copy(public_path('images/lluvia.jpg'), public_path('main.jpg'));
+        }
+
+        if ($response['main']['temp'] >= 23) {
+            File::copy(public_path('images/soleado.jpg'), public_path('main.jpg'));
+        }
         return $response;
     }
 }
